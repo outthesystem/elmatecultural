26864aedf48f9d609dab133ad56c9f774560d2d0
outthesystem <jet.market.dev@gmail.com>
2018-03-30 22:04:26 -0300

    cards edited

--- resources/views/Frontend/posts.blade.php
+++ resources/views/Frontend/posts.blade.php
@@ -7,8 +7,8 @@
       <form class="" action="{{route('front.index')}}" method="get">
         <div class="form-group" style="margin-top:-50px;">
           <h3>Selecciona una fecha para ver los eventos disponibles</h3>
-            <input type="text" id="datetimepicker" name="search" class="form-control datetimepicker" required autofocus/>
-            <input type="submit" class="btn btn-success" value="Buscar">
+          <input type="text" id="datetimepicker" name="search" class="form-control datetimepicker" required autofocus/>
+          <input type="submit" class="btn btn-success" value="Buscar">
         </div>
       </form>
     </center>
@@ -18,38 +18,58 @@
   @forelse ($posts as $p)
   <div class="col-sm-6">
 
-                  <div class="card card-blog">
-	    							<div class="card-image">
-	    								<a href="#pablo">
+    <div class="card card-blog">
+      <div class="card-image">
+        <a href="#pablo">
 	    									<img class="img" style="height:330px;" src="{{$p->image}}">
 	    									<div class="card-title">
 	    										{{$p->title}}
 	    									</div>
 	    								</a>
-	    							<div class="colored-shadow" style="background-image: url(&quot;{{$p->image}}&quot;); opacity: 1;height:330px;"></div><div class="ripple-container"></div></div>
+        <div class="colored-shadow" style="background-image: url(&quot;{{$p->image}}&quot;); opacity: 1;height:330px;"></div>
+        <div class="ripple-container"></div>
+      </div>
 
-	    							<div class="card-content">
-	    								<h6 class="category text-info">{{$p->category->name}}</h6>
-	    								<p class="card-description">
-                        {!! $p->description !!}
-	    								</p>
-	    							</div>
-	    						</div>
+      <div class="card-content">
+        <h6 class="category text-info">{{$p->category->name}}</h6>
+        <p class="card-description">
+          {!! $p->description !!}
+
+        </p>
+        <div class="footer">
+          <div class="author">
+            <a href="#pablo">
+	    	      <b class="text-success"><i class="material-icons">attach_money</i> {{$p->entrytype}} - <span>${{ number_format($p->price, 2) }}</span></b>
+	    	     </a>
+             <br>
+             <b class="text-info"><i class="material-icons">place</i> {{$p->place}}</b>
+
+          </div>
+          <div class="stats">
+            <b class="text-danger"><i class="material-icons">date_range</i> {{date('d/m/y', strtotime($p->date_init))}}
+              <br>
+            <i class="material-icons">schedule</i> {{$p->hour}}</b>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-@empty
+  @empty
   <div class="col-sm-12">
     <div class="card">
-	    <center><div class="card-content content-success">
-        <h4 class="card-title">
+      <center>
+        <div class="card-content content-success">
+          <h4 class="card-title">
           <a href="#pablo">No se han encontrado resultados.</a>
         </h4>
-        <p class="card-description">
-          Vuelve a intentarlo con otra fecha.
-        </p>
-      </div>
-    </div></center>
+          <p class="card-description">
+            Vuelve a intentarlo con otra fecha.
+          </p>
+        </div>
     </div>
-@endforelse
+    </center>
+  </div>
+  @endforelse
 
 </div>
 
